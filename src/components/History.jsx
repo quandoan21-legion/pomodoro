a3297a9f494a4c6699cbfe9d97cf996e9e8f2cc9
quandoan21-legion <qdoan21.legion@gmail.com>
2024-07-27 18:17:33 +0700

    feature xoa history

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -14,19 +14,30 @@ function History(props) {
         return timeStamp.toString().padStart(2, '0')
     }
 
+    function deleteHistory(historyId){
+        alert("You will delete history with ID: "+historyId);
+        db.pomodoros.delete(historyId)
+    }
+
     console.log(pomodoroObj)
     console.log(userObj)
     return (
         <ul id={"history-table"}>
                 <table id={"history-table"}>
                     <tr>
+                        <th>History ID</th>
                         <th>Start Time</th>
                         <th>End Time</th>
+                        <th>Delete History</th>
                     </tr>
             {pomodoroObj?.map((pomodoro) => (
                     <tr>
+                        <td>{pomodoro.id}</td>
                         <td>{beautifier(pomodoro.start.getHours())}:{beautifier(pomodoro.start.getMinutes())}:{beautifier(pomodoro.start.getSeconds())}</td>
                         <td>{beautifier(pomodoro.end.getHours())}:{beautifier(pomodoro.end.getMinutes())}:{beautifier(pomodoro.end.getSeconds())}</td>
+                        <td><div className={"delete-button"} onClick={() =>{
+                            deleteHistory(pomodoro.id)
+                        }}>Delete</div></td>
                     </tr>
             ))}
                 </table>
